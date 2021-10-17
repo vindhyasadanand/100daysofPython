756da84191c2faddeb481d61c4035ed60cdee4b2
vindhya Hegde <vindhyasadanand31@gmail.com>
2021-10-17 10:43:23 +0530

    Add/lifeinweeks (#3)
    * life in weeks

---
+++ Life in weeks/life.py
@@ -0,0 +1,8 @@
+print("Welcome!! Glad that you want to know how many weeks left")
+age = int(input("what is your age?"))
+years_remaining = 90 - age
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+print(f"You have {days_remaining} days {weeks_remaining} weeks {months_remaining} months {years_remaining} years remaining ")
+
